6a8f1940f47bb53cf8c3943617ee1cc1358dacb9
Vishwajeet Kumar <vishwajeet@localhost.localdomain>
2022-04-01 13:15:27 +0530

    This is a test file

---
+++ test.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int a , b, z;
+    a = 12;
+    b = 6;
+    z = a + b; 
+    printf(" The addition of two number is %d.\n",z);
+    z = a - b;
+    printf(" The subtration of two number is %d.\n",z);
+    z = a * b;
+    printf(" The Multiplication of two number is %d.\n",z);
+    z = a / b;
+    printf(" The quotient of two number is %d.\n",z);
+    return 0;
+}
