f531823679793f33dcf26019236592a16f88059a
Vishwajeet <vcnvim@gmail.com>
2022-04-22 07:41:54 +0530

    Renamed name of file

---
+++ test.c
@@ -0,0 +1,15 @@
+/* 6. Write a C program to compute the perimeter and area of a circle with a given radius. Go to the editor
+Expected Output:
+Perimeter of the Circle = 37.680000 inches
+Area of the Circle = 113.040001 square inches */
+
+
+
+// Author : vishwjeet kumar
+
+#include<stdio.h>
+
+int main(){
+    
+    return 0;
+}
