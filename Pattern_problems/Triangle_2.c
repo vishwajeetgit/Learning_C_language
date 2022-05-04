e4c16c5481ca3681859b9c4836c2d2c224ae782b
Vishwajeet <vcnvim@gmail.com>
2022-05-04 18:01:45 +0530

    Triangle_2

---
+++ Pattern_problems/Triangle_2.c
@@ -0,0 +1,41 @@
+/*
+ 
+Q. Write a program to print this type of pattern.
+
+Author: Vishwajeet kumar.
+
+
+#Expected output:
+
+Enter a number :)8
+       *
+      ***
+     *****
+    *******
+   *********
+  ***********
+ *************
+***************
+
+*/
+
+//Author: vishwajeet kumar
+
+
+#include<stdio.h>
+
+int main(){
+    int i , j, num;
+    printf("Enter a number:)");
+    scanf("%d",&num);
+    for(i = 1 ; i <= num; i++){
+	for(j = 1; j <= num+i-1;j++){
+	    if(i+j <= num)
+		printf(" ");
+	    else
+		printf("*");
+	}
+	printf("\n");
+    }
+    return 0;
+}
