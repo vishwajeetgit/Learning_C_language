1e46ce33b8aa99cf0edfec36da317778645699e3
Vishwajeet <vcnvim@gmail.com>
2022-05-04 11:06:38 +0530

    Patter of equilateral triangle

---
+++ Pattern_of_equilateral_triangle.c
@@ -0,0 +1,47 @@
+
+
+
+
+//Author : Vishwajeet kumar
+//Write a program to print this type of triangle.
+ 
+//Expected output::
+
+/*
+Enter a number:) 9
+        *
+       * *
+      *   *
+     *     *
+    *       *
+   *         *
+  *           *
+ *             *
+*****************
+*/
+
+
+//Author : Vishwajeet kumar
+//logic and code is purly by made by vishwajeet.
+
+
+
+
+#include<stdio.h>
+
+int main(){
+    int num;
+    int i, j, k;
+    printf("Enter a number:)");
+    scanf("%d",&num);
+    for(i = 1; i <= num; i++){
+	for (j = 1; j <= num + i - 1; j++){
+	    if(i+j == num+1 ||  j == num + i -1 || i == num)
+		printf("*");
+	    else
+		printf(" ");
+	}
+	printf("\n");
+    }
+    return 0;
+}
