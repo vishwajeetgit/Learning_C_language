730a30f7e8f2bb5ecdb02c9f7fd2ecc27f5dae99
Vishwajeet <vcnvim@gmail.com>
2022-05-09 06:17:06 +0530

    HCF

---
+++ Number_problems/HCF.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/*
+Q. program to HCF
+
+
+
+
+int main(){
+    int x , y ,r;
+    x = 25;
+    y = 320;
+    r = y%x;
+    while(r != 0){
+	y = x;
+	x = r;
+	r = y%x;
+    }
+    printf("%d\n", x);
+    return 0;
+}
+*/
+
+
+
+int main(){
+    int x , y;
+    x = 3;
+    y = 2;
+    printf("%d\n", y%x);
+
+    return 0;
+}
